b8fac5e6b8a529aa530c5868a4fb08eb0c12989e
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-04 10:41:50 -0500

    Add files via upload

--- Simulacro#1 Dados.cs
+++ Simulacro#1 Dados.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace simulacro
+namespace ConsoleApp47
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random aleatorio = new Random();
-            int dado1 = 0, dado2 = 0, dadoAd1 = 0, dadoAd2 = 2, total = 0;
+            int dado1 = 0, dado2 = 0, dadoAd1 = 0, contador = 1, dadoAd2 = 0, sumadados = 0, porcentaje = 0, total = 0, contadorbonus = 0;
             string continuar = "s";
+
+            bool A = false;
 
             dado1 = aleatorio.Next(1, 7);
             dado2 = aleatorio.Next(1, 7);
@@ -21,13 +23,11 @@ namespace simulacro
             total = dado1 + dado2;
             Console.Write("\nllevas = " + total);
 
-            bool A = false;
 
             if (dado1 == 1 && dado2 == 1)
             {
                 total = 0;
                 Console.WriteLine("\n  (Eliminado)");
-                A = true;
             }
             while (A != true && dado1 != 1 && dado2 != 1)
             {
@@ -35,22 +35,43 @@ namespace simulacro
                 continuar = Console.ReadLine();
                 if (continuar == "s")
                 {
+                    contador++;
                     dadoAd1 = aleatorio.Next(1, 7);
                     dadoAd2 = aleatorio.Next(1, 7);
                     Console.Write("dadoAd1 = " + dadoAd1);
                     Console.Write("   dadoAd2 = " + dadoAd2);
-                    total = dadoAd1 +dadoAd2 + total;
+                    total = dadoAd1 + dadoAd2 + total;
                     Console.WriteLine("\n\nllevas = " + total);
-                
                     if (dadoAd1 == 1 && dadoAd2 == 1)
                     {
                         total = 0;
                         Console.WriteLine("\n  (Eliminado)");
-                        A = true;
+                        porcentaje = (sumadados * 100) / contador;
+                        Console.WriteLine("\nPorcentaje = " + porcentaje);
                     }
-                    else if (total == 100 || total > 100)
+                    else if (total >= 100)
                     {
-                        total = + total;
+                        total = +total;
+                        Console.WriteLine(" (Ganas)");
+                        continuar = "n";
+                        A = true;
+                        porcentaje = (sumadados * 100) / contador;
+                        Console.WriteLine("\nPorcentaje = " + porcentaje);
+                    }
+                    if ((dado1 + dado2) > 6 || (dadoAd1 + dadoAd2) > 6)
+                    {
+                        sumadados++;
+                    }
+                    dado1 = 0;
+                    dado2 = 0;
+                    //para hacer que sean consecutivos
+                    if (dado1 == dado2 || dadoAd1 == dadoAd2)
+                    {
+                        contadorbonus++;
+                    }
+                    if (contadorbonus == 3)
+                    {
+                        total = +total;
                         Console.WriteLine(" (Ganas)");
                         continuar = "n";
                         A = true;
@@ -58,9 +79,12 @@ namespace simulacro
                 }
                 if (continuar == "n")
                 {
+                    porcentaje = (sumadados * 100) / contador;
+                    Console.WriteLine("\nPorcentaje = " + porcentaje);
                     Console.WriteLine("total = " + total);
                     A = true;
                 }
+                Console.Write("\nTurno = " + contador);
             }
         }
     }

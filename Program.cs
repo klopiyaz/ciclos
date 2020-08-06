b3aafad56be30fdf817f590ca770908d399a27a6
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-06 15:46:36 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp49
+namespace ConsoleApp50
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random aleatorio = new Random();
-            int dado1 = 0, dadoAd1 = 0, contador = 1, total = 0;
+            int dado1 = 0, dadoAd1 = 0, contador = 1, contadorbonus = 0, total = 0;
             string continuar = "s";
 
             bool A = false;
@@ -28,9 +28,10 @@ namespace ConsoleApp49
                 if (continuar == "s")
                 {
                     contador++;
-                    dadoAd1 = aleatorio.Next(1, 13);
+                    //dadoAd1 = aleatorio.Next(1, 13);
+                    dadoAd1 = int.Parse(Console.ReadLine());
                     Console.Write("\ndadoAd1 = " + dadoAd1);
-                    total = dadoAd1 + total;
+                    total += dadoAd1;
                     Console.WriteLine("\n\nllevas = " + total);
                     if (contador >= 3)
                     {
@@ -50,9 +51,22 @@ namespace ConsoleApp49
                     }
                     if (continuar == "n")
                     {
-
                         Console.WriteLine("total = " + total);
                         A = true;
+                    }
+                    if (dadoAd1 == 12)
+                    {
+                        contadorbonus++;
+                    }
+                    else if (dadoAd1 == 10 && contadorbonus == 1)
+                    {
+                        Console.WriteLine(" (Ganas)");
+                        continuar = "n";
+                        A = true;
+                    }
+                    else
+                    {
+                        contadorbonus = 0;
                     }
                     Console.Write("\nTurno = " + contador);
                 }

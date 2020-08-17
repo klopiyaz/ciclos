33d4ce3a04cebdd86b95f4256b9a91b0e28ed9a4
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-17 16:02:12 -0500

    Add files via upload

---
+++ ciclos for ejercicio 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1.25;
+            double sinx = 0;
+            int n = 100;
+
+            for (int i = 0; i <= n; i++)
+            {
+                sinx += ((Math.Pow((-1), i)) / fact((2*i)+1))* Math.Pow(x,((2*i)+1));
+            }
+            Console.WriteLine("sin = " + sinx);
+        }
+        static double fact(double i)
+        {
+            if (i <= 1) return 1;
+            return i * fact(i - 1);
+        }
+    }
+}

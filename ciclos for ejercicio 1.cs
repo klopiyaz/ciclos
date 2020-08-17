4c9d82b9f04eb02a843decd6b4c57425b46cc569
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-17 16:01:32 -0500

    Add files via upload

---
+++ ciclos for ejercicio 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp59
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 100;
+            int x = 3;
+            double E = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                E += Math.Pow(x, i) / fact(i);
+            }
+            Console.WriteLine("E = " + E);
+        }
+        static double fact(double i)
+        {
+            if (i <= 1) return 1;
+            return i * fact(i - 1);
+        }
+    }
+}
